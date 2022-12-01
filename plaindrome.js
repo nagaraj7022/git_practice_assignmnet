0237bd31e01d450c29a4026744807a9f3b70bfda
nagaraj7022 <prajwalreddy769@gmail.com>
2022-12-01 13:46:51 +0530

    i make code

---
+++ plaindrome.js
@@ -0,0 +1,11 @@
+let str = 'naman'
+let rev = ''
+
+for(let i=str.length-1; i>=0; i++){
+    rev += str[i]
+}
+if(str=rev){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
---
+++ prime.js
@@ -0,0 +1,11 @@
+let num = 13
+c=0
+for(i=1;i<=num;i++){'
+
+ c++
+}
+if(c==2){
+    console.log("prime")
+}else{
+    console.log("No")
+}
